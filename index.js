eeddb668bb444de9ec5bba882e21ea74d60032fe
Zoe Mendez <zoemendez316@gmail.com>
2022-07-22 10:57:54 -0600

    commit tree b1c2a6e513d373c636aebbfcf7616c9a98640078

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+const findMatching = (drivers, name) => {
+   return drivers.filter(item => item.toUpperCase() === name.toUpperCase())
+}
+const fuzzyMatch = (drivers, name) => {
+    return drivers.filter(item => item[0] === name[0])
+}
+const matchName = (drivers, string) => {
+    return drivers.filter(item => item.name === string )
+}
